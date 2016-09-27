9514d745620ae4424327539526a3e1eda8fdebb6
Beda Bogdan <bogdan.beda@hotmail.com>
2016-09-27 16:03:23 +0300

    - added quick tweaks to improve performance

--- UdgerParser/UdgerParser.cs
+++ UdgerParser/UdgerParser.cs
@@ -30,6 +30,10 @@ namespace Udger.Parser
         public string ip { get; set; }
         public string ua { get; set; }
 
+        private DataTable clientRegex;
+        private DataTable osRegex;
+        private DataTable device;
+
         /// <summary>
         /// Constructor 
         /// </summary> 
@@ -75,6 +79,27 @@ namespace Udger.Parser
         #endregion
 
         #region public method    
+
+        public void ConnectAndInit()
+        {
+            dt.connect(this);
+            this.clientRegex = dt.selectQuery(@"SELECT class_id,client_id,regstring,name,name_code,homepage,icon,icon_big,engine,vendor,vendor_code,vendor_homepage,uptodate_current_version,client_classification,client_classification_code
+                                              FROM udger_client_regex
+                                              JOIN udger_client_list ON udger_client_list.id = udger_client_regex.client_id
+                                              JOIN udger_client_class ON udger_client_class.id = udger_client_list.class_id
+                                              ORDER BY sequence ASC");
+
+            this.osRegex = dt.selectQuery(@"SELECT os_id,regstring,family,family_code,name,name_code,homepage,icon,icon_big,vendor,vendor_code,vendor_homepage
+                                              FROM udger_os_regex
+                                              JOIN udger_os_list ON udger_os_list.id = udger_os_regex.os_id
+                                              ORDER BY sequence ASC");
+
+            this.device = dt.selectQuery(@"SELECT deviceclass_id,regstring,name,name_code,icon,icon_big
+                                              FROM udger_deviceclass_regex
+                                              JOIN udger_deviceclass_list ON udger_deviceclass_list.id=udger_deviceclass_regex.deviceclass_id
+                                              ORDER BY sequence ASC");
+        }
+
         /// <summary>
         /// Parse the useragent string and/or ip address
         /// /// </summary>       
@@ -83,16 +108,16 @@ namespace Udger.Parser
             this.ipAddress = new IPAddress();
             this.userAgent = new UserAgent();
 
-            dt.connect(this);
+
             if (dt.Connected)
             {
                 if (this.ua != "")
-                {                    
+                {
                     this.parseUA(this.ua.Replace("'", "''"));
                     this.ua = "";
                 }
                 if (this.ip != "")
-                {                    
+                {
                     this.parseIP(this.ip.Replace("'", "''"));
                     this.ip = "";
                 }
@@ -134,11 +159,7 @@ namespace Udger.Parser
                     else
                     {
                         //client
-                        DataTable clientRegex = dt.selectQuery(@"SELECT class_id,client_id,regstring,name,name_code,homepage,icon,icon_big,engine,vendor,vendor_code,vendor_homepage,uptodate_current_version,client_classification,client_classification_code
-                                              FROM udger_client_regex
-                                              JOIN udger_client_list ON udger_client_list.id = udger_client_regex.client_id
-                                              JOIN udger_client_class ON udger_client_class.id = udger_client_list.class_id
-                                              ORDER BY sequence ASC");
+
 
                         if (clientRegex != null)
                             foreach (DataRow row in clientRegex.Rows)
@@ -163,10 +184,6 @@ namespace Udger.Parser
                             }
                     }
                     // OS
-                    DataTable osRegex = dt.selectQuery(@"SELECT os_id,regstring,family,family_code,name,name_code,homepage,icon,icon_big,vendor,vendor_code,vendor_homepage
-                                              FROM udger_os_regex
-                                              JOIN udger_os_list ON udger_os_list.id = udger_os_regex.os_id
-                                              ORDER BY sequence ASC");
                     if (osRegex != null)
                         foreach (DataRow row in osRegex.Rows)
                         {
@@ -192,10 +209,7 @@ namespace Udger.Parser
                         }
                     }
                     // device
-                    DataTable device = dt.selectQuery(@"SELECT deviceclass_id,regstring,name,name_code,icon,icon_big
-                                              FROM udger_deviceclass_regex
-                                              JOIN udger_deviceclass_list ON udger_deviceclass_list.id=udger_deviceclass_regex.deviceclass_id
-                                              ORDER BY sequence ASC");
+
 
                     if (device != null)
                         foreach (DataRow row in device.Rows)
